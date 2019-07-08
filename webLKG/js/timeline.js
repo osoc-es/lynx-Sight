9214857634b549b984de84b0e9652fbb81054c72
Alejandro <100330763@alumnos.uc3m.es>
2019-07-08 20:52:19 +0200

    First steps svg and js coding  08/07/2019 update: chosen svg approach to solution in contrast to HTML canvas because of interactivity.

---
+++ webLKG/js/timeline.js
@@ -0,0 +1,386 @@
+var hardcodedJSON = '{ "nombre" : "Alejandro", "apellido": "Rey"}';
+
+hardcodedJSON = "[ \
+  {    \
+    \"ATTRIBUTES\": [    \
+      {   \
+        \"TEXT\": \"REFERENCE for a preliminary ruling\",  \
+        \"END\": 5005,  \
+        \"BEGIN\": 4962, \
+        \"INFO\": \"what\" \
+      } \
+    ], \
+    \"CLASIFICATION\": \"event\", \
+    \"RELEVANCE\": 1, \
+    \"TEXT\": \"REFERENCE for a preliminary ruling\", \
+    \"END\": 5005, \
+    \"ID\": \"event 1\", \
+    \"BEGIN\": 4962, \
+    \"INFO\": \"what\", \
+    \"TYPE\": \"procedural\" \
+  }, \
+  { \
+    \"ATTRIBUTES\": [ \
+      { \
+        \"TEXT\": \"disagreement as to the scope of th\", \
+        \"END\": 24224, \
+        \"BEGIN\": 23937, \
+        \"INFO\": \"what \" \
+      } \
+    ], \
+    \"CLASIFICATION\": \"event\", \
+    \"RELEVANCE\": 1, \
+    \"TEXT\": \"disagreement as to the scope of th\", \
+    \"END\": 24224, \
+    \"ID\": \"event 2\", \
+    \"BEGIN\": 23937, \
+    \"INFO\": \"what \", \
+    \"TYPE\": \"facts under judgment\" \
+  }, \
+  { \
+    \"ATTRIBUTES\": [ \
+      { \
+        \"TEXT\": \"the latter companies (GoYellow and\", \
+        \"END\": 24348, \
+        \"BEGIN\": 24226, \
+        \"INFO\": \"what \" \
+      }, \
+      { \
+        \"TEXT\": \"GoYellow and Telix\", \
+        \"END\": 24224, \
+        \"BEGIN\": 24206, \
+        \"INFO\": \"whom\" \
+      }, \
+      { \
+        \"TEXT\": \"Deutsche Telekom\", \
+        \"END\": 24167, \
+        \"BEGIN\": 24151, \
+        \"INFO\": \"against whom\" \
+      }, \
+      { \
+        \"TEXT\": \"before the Bundesnetzagentur \", \
+        \"END\": 24295, \
+        \"BEGIN\": 24278, \
+        \"INFO\": \"before which court\" \
+      } \
+    ], \
+    \"CLASIFICATION\": \"event\", \
+    \"RELEVANCE\": 1, \
+    \"TEXT\": \"the latter companies (GoYellow and\", \
+    \"END\": 24348, \
+    \"ID\": \"event 3\", \
+    \"BEGIN\": 24226, \
+    \"INFO\": \"what \", \
+    \"TYPE\": \"background procedural\" \
+  }, \
+  { \
+    \"ATTRIBUTES\": [ \
+      { \
+        \"TEXT\": \"By decision of 11 September 2006, \", \
+        \"END\": 24882, \
+        \"BEGIN\": 24446, \
+        \"INFO\": \"what \" \
+      }, \
+      { \
+        \"TEXT\": \" 11 September 2006\", \
+        \"END\": 24483, \
+        \"BEGIN\": 24461, \
+        \"INFO\": \"when\" \
+      }, \
+      { \
+        \"TEXT\": \"Deutsche Telekom\", \
+        \"END\": 24532, \
+        \"BEGIN\": 24516, \
+        \"INFO\": \"against whom\" \
+      } \
+    ], \
+    \"CLASIFICATION\": \"event\", \
+    \"RELEVANCE\": 1, \
+    \"TEXT\": \"By decision of 11 September 2006, \", \
+    \"END\": 24882, \
+    \"ID\": \"event 4\", \
+    \"BEGIN\": 24446, \
+    \"INFO\": \"what \", \
+    \"TYPE\": \"background procedural\" \
+  }, \
+  { \
+    \"ATTRIBUTES\": [ \
+      { \
+        \"TEXT\": \"Deutsche Telekom brought an action\", \
+        \"END\": 25130, \
+        \"BEGIN\": 24980, \
+        \"INFO\": \"what \" \
+      }, \
+      { \
+        \"TEXT\": \"Deutsche Telekom\", \
+        \"END\": 24996, \
+        \"BEGIN\": 24980, \
+        \"INFO\": \"whom\" \
+      }, \
+      { \
+        \"TEXT\": \"Verwaltungsgericht Köln (Administr\", \
+        \"END\": 25083, \
+        \"BEGIN\": 25027, \
+        \"INFO\": \"before which court\" \
+      } \
+    ], \
+    \"CLASIFICATION\": \"event\", \
+    \"RELEVANCE\": 1, \
+    \"TEXT\": \"Deutsche Telekom brought an action\", \
+    \"END\": 25130, \
+    \"ID\": \"event 5\", \
+    \"BEGIN\": 24980, \
+    \"INFO\": \"what \", \
+    \"TYPE\": \"background procedural\" \
+  }, \
+  { \
+    \"ATTRIBUTES\": [ \
+      { \
+        \"TEXT\": \"By judgment of 14 February 2008, t\", \
+        \"END\": 25311, \
+        \"BEGIN\": 25228, \
+        \"INFO\": \"what \" \
+      }, \
+      { \
+        \"TEXT\": \"14 February 2008\", \
+        \"END\": 25259, \
+        \"BEGIN\": 25243, \
+        \"INFO\": \"when\" \
+      }, \
+      { \
+        \"TEXT\": \"the Verwaltungsgericht Köln\", \
+        \"END\": 25290, \
+        \"BEGIN\": 25262, \
+        \"INFO\": \"whom\" \
+      }, \
+      { \
+        \"TEXT\": \"Deutsche Telekom\", \
+        \"END\": 24996, \
+        \"BEGIN\": 24980, \
+        \"INFO\": \"against whom\" \
+      } \
+    ], \
+    \"CLASIFICATION\": \"event\", \
+    \"RELEVANCE\": 1, \
+    \"TEXT\": \"By judgment of 14 February 2008, t\", \
+    \"END\": 25311, \
+    \"ID\": \"event 6\", \
+    \"BEGIN\": 25228, \
+    \"INFO\": \"what \", \
+    \"TYPE\": \"background procedural\" \
+  }, \
+  { \
+    \"ATTRIBUTES\": [ \
+      { \
+        \"TEXT\": \"Deutsche Telekom then\\nappealed on \", \
+        \"END\": 25425, \
+        \"BEGIN\": 25313, \
+        \"INFO\": \"what \" \
+      }, \
+      { \
+        \"TEXT\": \"Deutsche Telekom \", \
+        \"END\": 25329, \
+        \"BEGIN\": 25313, \
+        \"INFO\": \"whom\" \
+      }, \
+      { \
+        \"TEXT\": \"Bundesverwaltungsgericht (Federal \", \
+        \"END\": 25425, \
+        \"BEGIN\": 25370, \
+        \"INFO\": \"before which court\" \
+      } \
+    ], \
+    \"CLASIFICATION\": \"event\", \
+    \"RELEVANCE\": 1, \
+    \"TEXT\": \"Deutsche Telekom then\\nappealed on \", \
+    \"END\": 25425, \
+    \"ID\": \"event 7\", \
+    \"BEGIN\": 25313, \
+    \"INFO\": \"what \", \
+    \"TYPE\": \"background procedural\" \
+  }, \
+  { \
+    \"ATTRIBUTES\": [ \
+      { \
+        \"TEXT\": \"the Bundesverwaltungsgericht decid\", \
+        \"END\": 28274, \
+        \"BEGIN\": 28138, \
+        \"INFO\": \"what \" \
+      }, \
+      { \
+        \"TEXT\": \"the Bundesverwaltungsgerich\", \
+        \"END\": 28166, \
+        \"BEGIN\": 28138, \
+        \"INFO\": \"whom\" \
+      }, \
+      { \
+        \"TEXT\": \" to the Court\", \
+        \"END\": 28249, \
+        \"BEGIN\": 28240, \
+        \"INFO\": \"before which court\" \
+      } \
+    ], \
+    \"CLASIFICATION\": \"event\", \
+    \"RELEVANCE\": 1, \
+    \"TEXT\": \"the Bundesverwaltungsgericht decid\", \
+    \"END\": 28274, \
+    \"ID\": \"event 8\", \
+    \"BEGIN\": 28138, \
+    \"INFO\": \"what \", \
+    \"TYPE\": \"background procedural\" \
+  }, \
+  { \
+    \"ATTRIBUTES\": [ \
+      { \
+        \"TEXT\": \"As the Advocate General observed i\", \
+        \"END\": 48773, \
+        \"BEGIN\": 48399, \
+        \"INFO\": \"what \" \
+      } \
+    ], \
+    \"CLASIFICATION\": \"event\", \
+    \"RELEVANCE\": 1, \
+    \"TEXT\": \"As the Advocate General observed i\", \
+    \"END\": 48773, \
+    \"ID\": \"event 9\", \
+    \"BEGIN\": 48399, \
+    \"INFO\": \"what \", \
+    \"TYPE\": \"procedural\" \
+  }, \
+  { \
+    \"CLASIFICATION\": \"event\", \
+    \"TEXT\": \"On those grounds, the Court (Third\", \
+    \"END\": 54429, \
+    \"ID\": \"event 10\", \
+    \"BEGIN\": 54373, \
+    \"INFO\": \"what  (final ruling)\", \
+    \"TYPE\": \"procedural\" \
+  } \
+]"; 
+
+//ICONS to be used in timeline:
+const iconProcedural = new Image(); 
+iconProcedural.src = "img/Procedural.png";
+
+const iconBackProcedural = new Image();
+iconBackProcedural.src="img/BackgroundProcedural.png";
+
+const factsUnderJudg = new Image();
+factsUnderJudg.src="img/FactsUnderJudgement.png";
+
+
+
+//Nos dan un array de eventos
+	class Levent  {
+		constructor(attributes, classification, relevance, text, end, id, begin, type){
+			this.attributes = attributes;
+			this.classification = classification;//Esto es innecesario de momento no? Sería para hacer referencia a EVENT, METADATA etc.
+			this.relevance= relevance; //num Entero que indica relevancia (the bigger the more important?) 1 (most important!)
+			this.text = text; //String que contiene todo el texto que contiene el resaltado
+			this.end = end; // Número de word count.
+			this.id = id;//Nombre identificativo --> String?
+			this.begin= begin; // word count? IDK
+			//info, // representa el conjunto de preguntas que se haría el usuario respecto a qué relación tiene este evento con el texto resaltado en general --> [IGNORE]
+			this.type= type; // Entiendo que aquí viene Procedural, Background Procedural y Facts under judgement
+		}
+	}
+
+
+
+var imageNodes = [];
+//console.log(iconBackProcedural);
+
+imageNodes[0] = iconBackProcedural;
+document.getElementById("imageDisplayer").appendChild(iconBackProcedural);
+imageNodes[1] = iconProcedural;
+document.getElementById("imageDisplayer").appendChild(iconProcedural);
+imageNodes[2] = factsUnderJudg;
+document.getElementById("imageDisplayer").appendChild(factsUnderJudg);
+
+console.log("%c imageNodes array: ","#ff6600",imageNodes);
+
+
+//document.getElementsByTagName("body")[0].style.backgroundColor='#ffff66';
+
+console.log("Beginning of the project");
+
+console.log("%c This is a number %d;'\n' this is a string  %s;\n this is a float (two decimal digits) %.2f",'color:blue', 15, "Freak", 0.77);
+
+
+//console.table(data); recibe un array o un object
+console.table(["Hello","Goodbye","Welcome"]);
+
+console.table({name:"Manolo",surname:"el del Bombo"});
+
+
+//Search within the HTML document to get a canvas instance  
+var canvas = document.querySelector('canvas');
+console.log(canvas);
+//Access the width and change it by script
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight/2;
+//Context is an object allowing "paint" onto the canvas
+var context = canvas.getContext("2d");
+
+	
+	//Drawing filled rectangles, those with color
+	context.fillStyle = 'red';
+	context.fillRect(0,0,10,10);
+	context.fillStyle = 'blue';
+	context.fillRect((window.innerWidth-100),0,100,100);
+	context.fillStyle = 'green';
+	context.fillRect((window.innerWidth/2),0,20,20);
+
+	//Draw rectangular strokes only
+	context.strokeStyle = 'black';
+	context.strokeRect(10,10,10,10);
+	context.strokeStyle = 'brown';
+	context.strokeRect((window.innerWidth-20),10,10,10);
+
+	//context.clearRect(0,0,window.innerWidth, window.innerHeight);
+
+	//1. Crear un camino (path)
+	context.beginPath();
+	//2. Especificar coordenadas donde comenzará dicho camino
+	context.moveTo((window.innerWidth/2),30);
+	//3. Crear forma:
+		context.lineTo((window.innerWidth/2)+6,50);
+		context.lineTo((window.innerWidth/2)-6,50);
+	//4. Cerrar camino
+		context.closePath();
+
+	//5.Dibujar camino
+		context.stroke();
+
+
+//1. Crear un camino (path)
+	context.beginPath();
+	//2. Especificar coordenadas donde comenzará dicho camino
+	//context.moveTo((window.innerWidth/2),60);
+	context.arc((window.innerWidth/2),60,5,0, 2*Math.PI );
+	//5.Dibujar camino
+		context.stroke();
+
+//Clear everything in the canvas
+context.clearRect(0,0,window.innerWidth,window.innerHeight);
+
+
+//Drawing images:
+
+//1.Draw image just as it is, given x,y coordinates of top left corner
+//context.drawImage(iconProcedural,(window.innerWidth/2),0);
+//2.Draw image given coordinates and width, height pair
+//context.drawImage(factsUnderJudg,0,0,30,30);
+//3.Draw images cropping part of them.
+context.drawImage(iconBackProcedural,40,40,0,0)
+
+
+console.log(iconBackProcedural);
+console.log(context);
+
+var eventArray = JSON.parse(hardcodedJSON);
+
+
+console.log("%c eventArray: %c","#429bf5","#278c1d",eventArray[0]);
+
+
